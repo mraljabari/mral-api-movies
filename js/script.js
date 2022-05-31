b929bf3e0e25c31a7cf941e78136fb693d915efb
mraljabri <hydeki.rizky@gmail.com>
2022-06-01 00:31:34 +0700

    modif

--- js/script.js
+++ js/script.js
@@ -7,30 +7,31 @@ $('#search-button').on('click', function () {
             'apikey': 'a8b50f2d',
             's': $('#search-input').val()
         },
-        success: function(data){
-            if(data.Response == "True"){
-                console.log(data);
-                let d = data.search
-
-                $.each(data, function(i, d){
+        success: function(result){
+            if(result.Response == "True"){
+                console.log(result);
+                let movies = result.Search;
+                $('#movie-list').empty(); 
+                $.each(movies, function(i, data){
                     $('#movie-list').append(`<div class="col-md-4">
                     <div class="card mb-3">
-                        <img src="`+ data.Search.Poster +`" class="card-img-top" alt="...">
+                        <img src="`+ data.Poster +`" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">`+ data.totalResult +` <<< ini title</h5>
-                            <p class="card-text">`+ data.totalResult +` Deskripsi</p>
-                            <p class="card-text"><b>Harga : Rp. Harga</b></p>
-                            <a href="#" class="btn btn-primary">Order Now</a>
+                            <h5 class="card-title">`+ data.Title +`</h5>
+                            <p class="card-text">Type : `+ data.Type +`</p>
+                            <p class="card-text"><b>Year : `+ data.Year +`</b></p>
+                            <a href="#" class="btn btn-primary">`+ data.imdbID +`</a>
                         </div>
                         </div>
                     </div>`)
                 });
             }else{
-                console.log(data.Error);
+                console.log(result.Error);
+                $('#movie-list').empty(); 
 
                 $('#movie-list').html(`
                     <div class="col">
-                        <h1 class=text-center>` + data.Error + `</h1>
+                        <h1 class=text-center>` + result.Error + `</h1>
                     </div>
                 `)
             }
